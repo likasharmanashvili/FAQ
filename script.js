5e6da81c1e099f7fc8f2e4ea25956364cf33f808
lika <l.sharmanashvili@ssu.edu.g>
2024-02-14 16:37:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,184 @@
+const ptag = document.querySelector(".ptag")
+const phide = document.querySelector(".phide")
+const icon1 = document.getElementById("icon1")
+
+
+
+icon1.addEventListener('click' , () => {
+    if(phide.style.display = 'none')
+    phide.style.display = 'block'
+    icon1.style.transform = 'rotate(180deg)'
+    ptag.style.fontWeight = '700'
+    
+    phide1.style.display = 'none'
+    icon2.style.transform = 'rotate(0deg)'
+    ptag1.style.fontWeight = '400'
+    
+    
+    phide2.style.display = 'none'
+    icon3.style.transform = 'rotate(0deg)'
+    ptag2.style.fontWeight = '400'
+
+    
+    phide3.style.display = 'none'
+    icon4.style.transform = 'rotate(0deg)'
+    ptag3.style.fontWeight = '400'
+
+      
+    phide4.style.display = 'none'
+    icon5.style.transform = 'rotate(0deg)'
+    ptag4.style.fontWeight = '400'
+
+    
+})
+
+
+const ptag1 = document.querySelector(".ptag1")
+const phide1 = document.querySelector(".phide1")
+const icon2 = document.getElementById("icon2")
+
+
+icon2.addEventListener('click' , () => {
+    if(phide1.style.display = 'none')
+    phide1.style.display = 'block'
+    icon2.style.transform = 'rotate(180deg)'
+    ptag1.style.fontWeight = '700'
+    
+    phide.style.display = 'none'
+    icon1.style.transform = 'rotate(0deg)'
+    ptag.style.fontWeight = '400'
+    
+    
+    phide2.style.display = 'none'
+    icon3.style.transform = 'rotate(0deg)'
+    ptag2.style.fontWeight = '400'
+
+    
+    phide3.style.display = 'none'
+    icon4.style.transform = 'rotate(0deg)'
+    ptag3.style.fontWeight = '400'
+
+      
+    phide4.style.display = 'none'
+    icon5.style.transform = 'rotate(0deg)'
+    ptag4.style.fontWeight = '400'
+
+  
+})
+
+
+
+
+
+
+
+const ptag2 = document.querySelector(".ptag2")
+const phide2 = document.querySelector(".phide2")
+const icon3 = document.getElementById("icon3")
+
+
+icon3.addEventListener('click' , () => {
+    if(phide2.style.display = 'none')
+    phide2.style.display = 'block'
+    icon3.style.transform = 'rotate(180deg)'
+    ptag2.style.fontWeight = '700'
+    
+     phide1.style.display = 'none'
+    icon2.style.transform = 'rotate(0deg)'
+    ptag1.style.fontWeight = '400'
+    
+    
+    phide.style.display = 'none'
+    icon2.style.transform = 'rotate(0deg)'
+    ptag.style.fontWeight = '400'
+
+    
+    phide3.style.display = 'none'
+    icon4.style.transform = 'rotate(0deg)'
+    ptag3.style.fontWeight = '400'
+
+      
+    phide4.style.display = 'none'
+    icon5.style.transform = 'rotate(0deg)'
+    ptag4.style.fontWeight = '400'
+    
+
+  
+})
+
+
+
+
+
+
+const ptag3 = document.querySelector(".ptag3")
+const phide3 = document.querySelector(".phide3")
+const icon4 = document.getElementById("icon4")
+
+
+icon4.addEventListener('click' , () => {
+    if(phide3.style.display = 'none')
+    phide3.style.display = 'block'
+    icon4.style.transform = 'rotate(180deg)'
+    ptag3.style.fontWeight = '700'
+    
+    phide1.style.display = 'none'
+    icon2.style.transform = 'rotate(0deg)'
+    ptag1.style.fontWeight = '400'
+    
+    
+    phide2.style.display = 'none'
+    icon3.style.transform = 'rotate(0deg)'
+    ptag2.style.fontWeight = '400'
+
+    
+    phide.style.display = 'none'
+    icon3.style.transform = 'rotate(0deg)'
+    ptag.style.fontWeight = '400'
+
+      
+    phide4.style.display = 'none'
+    icon5.style.transform = 'rotate(0deg)'
+    ptag4.style.fontWeight = '400'
+    
+
+  
+})
+
+
+
+
+
+
+const ptag4 = document.querySelector(".ptag4")
+const phide4 = document.querySelector(".phide4")
+const icon5 = document.getElementById("icon5")
+
+
+icon5.addEventListener('click' , () => {
+    if(phide4.style.display = 'none')
+    phide4.style.display = 'block'
+    icon5.style.transform = 'rotate(180deg)'
+    ptag4.style.fontWeight = '700'
+    
+    phide1.style.display = 'none'
+    icon2.style.transform = 'rotate(0deg)'
+    ptag1.style.fontWeight = '400'
+    
+    
+    phide2.style.display = 'none'
+    icon3.style.transform = 'rotate(0deg)'
+    ptag2.style.fontWeight = '400'
+
+    
+    phide3.style.display = 'none'
+    icon4.style.transform = 'rotate(0deg)'
+    ptag3.style.fontWeight = '400'
+
+      
+    phide.style.display = 'none'
+    icon4.style.transform = 'rotate(0deg)'
+    ptag.style.fontWeight = '400'
+
+  
+})
